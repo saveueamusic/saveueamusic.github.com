f19debca598ad720197bad70ca0af6225936ad40
Alex Sanders <sndrs@me.com>
2011-11-08 14:22:49 +0000

    hide ribbon from <IE9

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 */
 $(function() {
-	$('body').prepend('<a href="http://support.saveueamusic.org/" id="support_ribbon">Statements<br/>of support</a>');
+	$('html:not(.ie6, .ie7, .ie8) body').prepend('<a href="http://support.saveueamusic.org/" id="support_ribbon">Statements<br/>of support</a>');
 });
 
 
